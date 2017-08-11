3b42a526108f14d3b60b7cb926bbc5ac4b3bdc05
Pascal VINEY <viney@novius.fr>
2017-08-11 11:02:02 +0200

    Custom lang file

--- src/CrudPanel.php
+++ src/CrudPanel.php
@@ -6,9 +6,36 @@ use \Backpack\CRUD\CrudPanel as BackpackCrudPanel;
 
 class CrudPanel extends BackpackCrudPanel
 {
+    protected $lang_file;
+
     public function __construct()
     {
-        dump(' Novius\Backpack\CRUD\CrudPanel called!');
         parent::__construct();
+    }
+
+
+    /**
+     * Set the lang file to use
+     * Used all over the CRUD interface (header, add button, reorder button, breadcrumbs).
+     *
+     * @param [string] Entity name, in singular. Ex: article
+     * @param [string] Entity name, in plural. Ex: articles
+     */
+    public function setLangFile($path)
+    {
+        $this->lang_file = $path;
+    }
+
+    /**
+     * Get the entity strings
+     * Used all over the CRUD interface (header, add button, reorder button, breadcrumbs).
+     *
+     * @param [string] Entity name, in singular. Ex: article
+     * @param [string] Entity name, in plural. Ex: articles
+     * @return [array]
+     */
+    public function getLangFile()
+    {
+        return $this->lang_file ?? 'backpack::crud';
     }
 }
