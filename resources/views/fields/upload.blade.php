07b553b2596627a3bd92a60c2b71ea4fb3b93d0d
Tony BOTALLA <botalla@novius.fr>
2017-08-30 15:44:26 +0200

    Able to validate request + prevent duplicate input name

--- resources/views/fields/upload.blade.php
+++ resources/views/fields/upload.blade.php
@@ -18,14 +18,24 @@
         </div>
     @endif
 
-    {{-- Show the file picker on CREATE form. --}}
-    <input
-            type="file"
-            id="{{ $field['name'] }}_file_input"
-            name="{{ $field['name'] }}"
-            value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
-            @include('crud::inc.field_attributes', ['default_class' =>  isset($field['value']) && $field['value']!=null?'form-control hidden':'form-control'])
-    >
+    @if (isset($field['value']) && $field['value'] !== null)
+        {{-- File already exists : put the path value to hidden file  --}}
+        <input
+                type="hidden"
+                id="{{ $field['name'] }}_file_input"
+                name="{{ $field['name'] }}"
+                value="{{ $field['value'] }}"
+                @include('crud::inc.field_attributes', ['default_class' =>  'form-control']) />
+    @else
+        {{-- File not exists : display an input file --}}
+        <input
+                type="file"
+                id="{{ $field['name'] }}_file_input"
+                name="{{ $field['name'] }}"
+                value="{{ isset($field['default']) ? $field['default'] : '' }}"
+                @include('crud::inc.field_attributes', ['default_class' => 'form-control'])
+        />
+    @endif
 
     {{-- HINT --}}
     @if (isset($field['hint']))
@@ -37,23 +47,14 @@
 {{-- push things in the after_scripts section --}}
 
 @push('crud_fields_scripts')
-<!-- no scripts -->
-<script>
-    $("#{{ $field['name'] }}_file_clear_button").click(function(e) {
-        e.preventDefault();
-        $(this).parent().addClass('hidden');
-
-        var input = $("#{{ $field['name'] }}_file_input");
-        input.removeClass('hidden');
-        input.attr("value", "").replaceWith(input.clone(true));
-        // add a hidden input with the same name, so that the setXAttribute method is triggered
-        $("<input type='hidden' name='{{ $field['name'] }}' value=''>").insertAfter("#{{ $field['name'] }}_file_input");
-    });
-
-    $("#{{ $field['name'] }}_file_input").change(function() {
-        console.log($(this).val());
-        // remove the hidden input, so that the setXAttribute method is no longer triggered
-        $(this).next("input[type=hidden]").remove();
-    });
-</script>
+    <!-- no scripts -->
+    <script>
+        $("#{{ $field['name'] }}_file_clear_button").click(function(e) {
+            e.preventDefault();
+            $(this).parent().addClass('hidden');
+            // Replace input hidden by an input file and show it
+            var $input = $("#{{ $field['name'] }}_file_input");
+            $input.replaceWith($input.clone().attr('type', 'file').val('').removeClass('hidden'));
+        });
+    </script>
 @endpush
