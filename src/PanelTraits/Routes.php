544f59944f489a03acb88435decc88d7a7a91fb3
Tony BOTALLA <botalla@novius.fr>
2017-09-11 14:52:25 +0200

    Add reorder route

--- src/PanelTraits/Routes.php
+++ src/PanelTraits/Routes.php
@@ -5,6 +5,7 @@ namespace Novius\Backpack\CRUD\PanelTraits;
 trait Routes
 {
     protected $indexRoute;
+    protected $reorderRoute;
 
     /**
      * Set a custom index route
@@ -27,8 +28,34 @@ trait Routes
      *
      * @return string
      */
-    public function indexRoute() : string
+    public function indexRoute(): string
     {
         return $this->indexRoute ?? $this->route;
+    }
+
+    /**
+     * Set a reorder route
+     *
+     * @param $routeName
+     * @param array $parameters
+     * @throws \Exception
+     */
+    public function setReorderRoute($routeName, $parameters = [])
+    {
+        if (!\Route::has($routeName)) {
+            throw new \Exception('There are no routes for this route name.', 404);
+        }
+
+        $this->reorderRoute = route($routeName, $parameters);
+    }
+
+    /**
+     * Get the reorder route
+     *
+     * @return string
+     */
+    public function reorderRoute(): string
+    {
+        return $this->reorderRoute ?? $this->route.'/reorder';
     }
 }
